3ab5c36684ce13232978499b337d779906b6db54
spkl <spkl>
2023-12-09 11:08:11 +0100

    improved solution for day 7

---
+++ Day07/HandComparer.cs
@@ -0,0 +1,31 @@
+﻿internal abstract class HandComparer : IComparer<(string hand, int bid)>
+{
+    public abstract string Strengths { get; }
+
+    public abstract HandType GetType(string hand);
+
+    public int Compare((string hand, int bid) x, (string hand, int bid) y)
+    {
+        // Less than zero: x is less than y.
+        // Zero: x equals y.
+        // Greater than zero: x is greater than y.
+        HandType xType = this.GetType(x.hand);
+        HandType yType = this.GetType(y.hand);
+        if (xType != yType)
+        {
+            return xType - yType;
+        }
+
+        for (int i = 0; i < x.hand.Length; i++)
+        {
+            int xStrength = this.Strengths.IndexOf(x.hand[i]);
+            int yStrength = this.Strengths.IndexOf(y.hand[i]);
+            if (xStrength != yStrength)
+            {
+                return xStrength - yStrength;
+            }
+        }
+
+        return 0;
+    }
+}
--- Day07/PartOneComparer.cs
+++ Day07/PartOneComparer.cs
@@ -1,87 +1,19 @@
-internal class PartOneComparer : IComparer<(string hand, int bid)>
+internal class PartOneComparer : HandComparer
 {
-    private static readonly Dictionary<char, int> Strengths = new()
+    public override string Strengths { get; } = "23456789TJQKA";
+
+    public override HandType GetType(string hand)
     {
-        {'A', 14},
-        {'K', 13},
-        {'Q', 12},
-        {'J', 11},
-        {'T', 10},
-        {'9', 9},
-        {'8', 8},
-        {'7', 7},
-        {'6', 6},
-        {'5', 5},
-        {'4', 4},
-        {'3', 3},
-        {'2', 2},
-    };
-
-    public int Compare((string hand, int bid) x, (string hand, int bid) y)
-    {
-        // Less than zero: x is less than y.
-        // Zero: x equals y.
-        // Greater than zero: x is greater than y.
-        HandType xType = this.GetType(x.hand);
-        HandType yType = this.GetType(y.hand);
-        if (xType != yType)
+        int[] counts = hand.Distinct().Select(card => hand.Count(card2 => card == card2)).OrderDescending().ToArray();
+        return counts switch
         {
-            return xType - yType;
-        }
-
-        for (int i = 0; i < x.hand.Length; i++)
-        {
-            char xCard = x.hand[i];
-            char yCard = y.hand[i];
-            if (Strengths[xCard] != Strengths[yCard])
-            {
-                return Strengths[xCard] - Strengths[yCard];
-            }
-        }
-
-        return 0;
-    }
-
-    private HandType GetType(string hand)
-    {
-        Dictionary<char, int> counts = [];
-        foreach (char card in hand)
-        {
-            if (!counts.ContainsKey(card))
-            {
-                counts[card] = 0;
-            }
-
-            counts[card]++;
-        }
-
-        if (counts.Count == 1)
-        {
-            return HandType.FiveOfAKind;
-        }
-        else if (counts.ContainsValue(4))
-        {
-            return HandType.FourOfAKind;
-        }
-        else if (counts.ContainsValue(3) && counts.ContainsValue(2))
-        {
-            return HandType.FullHouse;
-        }
-        else if (counts.ContainsValue(3))
-        {
-            return HandType.ThreeOfAKind;
-        }
-        else if (counts.Values.Count(count => count == 2) == 2)
-        {
-            return HandType.TwoPair;
-        }
-        else if (counts.Values.Any(count => count == 2))
-        {
-            return HandType.OnePair;
-        }
-        else 
-        {
-            return HandType.HighCard;
-        }
+            [5] => HandType.FiveOfAKind,
+            [4, 1] => HandType.FourOfAKind,
+            [3, 2] => HandType.FullHouse,
+            [3, 1, 1] => HandType.ThreeOfAKind,
+            [2, 2, 1] => HandType.TwoPair,
+            [2, 1, 1, 1] => HandType.OnePair,
+            _ => HandType.HighCard,
+        };
     }
 }
